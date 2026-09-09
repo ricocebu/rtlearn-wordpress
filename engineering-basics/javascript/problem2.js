3f23dfea531e9f85946c8accaafb3781064a696c
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-09-09 14:26:06 +0800

    Solution and explaination added in the comments

--- engineering-basics/javascript/problem2.js
+++ engineering-basics/javascript/problem2.js
@@ -3,14 +3,17 @@
 let a = 3;
 let b = "3";
 
-console.log( a+b );
-console.log( b+a);
+console.log( a+b );   // 33
+console.log( b+a);    // 33
 
-console.log( b*a );
-console.log( a*b );
+console.log( b*a );   // 9
+console.log( a*b );   // 9
 
-console.log( a/b );
-console.log( b/a );
+console.log( a/b );   // 1
+console.log( b/a );   // 1
 
-console.log( a-b );
-console.log( b-a );
+console.log( a-b );   // 0
+console.log( b-a );   // 0
+
+// The addition output is 33 because it treats the addition as concatenation.
+// The multiplication, division, and subtraction converts any numeric string into a number.
