caf64b64279b6d5cc4b0416691c704c0c91828b0
ricocebu <124438758+ricocebu@users.noreply.github.com>
2026-09-09 14:05:47 +0800

    2nd problem in JS

---
+++ engineering-basics/javascript/problem2.js
@@ -0,0 +1,16 @@
+// 2. What will be the output of the following, and why?
+
+let a = 3;
+let b = "3";
+
+console.log( a+b );
+console.log( b+a);
+
+console.log( b*a );
+console.log( a*b );
+
+console.log( a/b );
+console.log( b/a );
+
+console.log( a-b );
+console.log( b-a );
